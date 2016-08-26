d10c69ef15c740f424654b56b90bda9888e0fa9a
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-08-26 09:01:15 -0700

    adds case large to sizePrice

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 function pizza(){
   this.small = 4.50;
   this.medium = 6.50;
+  this.large = 7.50;
 }
 pizza.prototype.sizePrice = function(size) {
   if (size === "small"){return this.small};
   if (size === "medium"){return this.medium}
+  if (size === "large"){return this.large}
 }
 $(document).ready(function(){
   var pizzaObject = new pizza;
